22a0092905297a2bab2bf5fbbe490c1850b29561
Solray78 <80864781+Solray78@users.noreply.github.com>
2021-04-15 10:06:23 +0900

    0415_30308

--- Assets/Scipts/ScrollingObject.cs
+++ Assets/Scipts/ScrollingObject.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class ScrollingObject : MonoBehaviour
 {
     public float speed = 10f;
+    
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover) {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        
     }
 }
